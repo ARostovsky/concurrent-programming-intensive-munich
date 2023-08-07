10c52b87b5e942694bdd7bad67947c313bc9ceb4
Aleksey Rostovskiy <aleksei.rostovskii@jetbrains.com>
2023-08-07 14:49:48 +0200

    Solution 4: TreiberStackWithElimination

--- src/day1/TreiberStackWithElimination.kt
+++ src/day1/TreiberStackWithElimination.kt
@@ -16,7 +16,6 @@ open class TreiberStackWithElimination<E> : Stack<E> {
     }
 
     protected open fun tryPushElimination(element: E): Boolean {
-        TODO("Implement me!")
         // TODO: Choose a random cell in `eliminationArray`
         // TODO: and try to install the element there.
         // TODO: Wait `ELIMINATION_WAIT_CYCLES` loop cycles
@@ -24,20 +23,36 @@ open class TreiberStackWithElimination<E> : Stack<E> {
         // TODO: element. If so, clean the cell and finish,
         // TODO: returning `true`. Otherwise, move the cell
         // TODO: to the empty state and return `false`.
+        val index = randomCellIndex()
+        if (!eliminationArray.compareAndSet(index, CELL_STATE_EMPTY, element)) {
+            return false
+        }
+        repeat(ELIMINATION_WAIT_CYCLES) {
+            if (eliminationArray.compareAndSet(index, CELL_STATE_RETRIEVED, CELL_STATE_EMPTY)) {
+                return true
+            }
+        }
+        val prevValue = eliminationArray.getAndSet(index, CELL_STATE_EMPTY)
+        return prevValue == CELL_STATE_RETRIEVED
     }
 
     override fun pop(): E? = tryPopElimination() ?: stack.pop()
 
     private fun tryPopElimination(): E? {
-        TODO("Implement me!")
         // TODO: Choose a random cell in `eliminationArray`
         // TODO: and try to retrieve an element from there.
         // TODO: On success, return the element.
         // TODO: Otherwise, if the cell is empty, return `null`.
+        val index = randomCellIndex()
+        val element = eliminationArray.get(index)
+        return when {
+            element == CELL_STATE_EMPTY || element == CELL_STATE_RETRIEVED -> null
+            eliminationArray.compareAndSet(index, element, CELL_STATE_RETRIEVED) -> element as E?
+            else -> null
+        }
     }
 
-    private fun randomCellIndex(): Int =
-        ThreadLocalRandom.current().nextInt(eliminationArray.length())
+    private fun randomCellIndex(): Int = ThreadLocalRandom.current().nextInt(eliminationArray.length())
 
     companion object {
         private const val ELIMINATION_ARRAY_SIZE = 2 // Do not change!
